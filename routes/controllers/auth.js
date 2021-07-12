185dfce82cc95a9fbd76288cadf4c6b8c90deabb
unknown <edisonalzate11@gmail.com>
2021-07-12 11:00:33 -0500

    custom midlewares

--- routes/controllers/auth.js
+++ routes/controllers/auth.js
@@ -5,14 +5,7 @@ const crearUsuario=(req,res=response)=>{
     
     const {name,email,password}=req.body
 
-    //manejo de errores
-    const errors=validationResult(req)
-    if(!errors.isEmpty()){
-       return res.status(400).json({
-           ok:false,
-           errors:errors.mapped()
-       }) 
-    }
+    
 
     res.status(201).json({
         ok:true,
@@ -25,13 +18,6 @@ const crearUsuario=(req,res=response)=>{
 
 const loginUsuario=(req,res=response)=>{
 
-    const errors=validationResult(req)
-    if(!errors.isEmpty()){
-       return res.status(400).json({
-           ok:false,
-           errors:errors.mapped()
-       }) 
-    }
     
     const {email,password}=req.body
     res.json({
