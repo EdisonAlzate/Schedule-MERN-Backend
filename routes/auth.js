7f8612f4c607ae51f1b9403d246cb2d63eb828fd
unknown <edisonalzate11@gmail.com>
2021-07-11 22:53:31 -0500

    crearUsuario, loginUsuario,revalidarToken routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
-const {Router}=require('express');
-const router=Router()
 
-router.get('/',(req,res)=>{
-    
-    res.json({
-        ok:true
-    })
-})
+/* rutas de usuarios/Auth
+   host+/api/auth
+*/
+
+
+const {Router}=require('express');
+const router=Router();
+
+const {crearUsuario,loginUsuario,
+    revalidarToken}=require('./controllers/auth')
+
+router.post('/new',crearUsuario)
+
+router.post('/',loginUsuario)
+
+router.get('/renew',revalidarToken)
 
 module.exports=router;
