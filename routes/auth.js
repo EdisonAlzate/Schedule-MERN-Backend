33ad5eedcc767e1d8c1988a1751ba5c820e33319
unknown <edisonalzate11@gmail.com>
2021-07-14 14:58:00 -0500

    token renew

--- routes/auth.js
+++ routes/auth.js
@@ -11,6 +11,9 @@ const router=Router();
 const {crearUsuario,loginUsuario,
     revalidarToken}=require('./controllers/auth');
 const { validarCampos } = require('./middlewares/validar-campos');
+const { validarJWT } = require('./middlewares/validar-jwt');
+
+
 
 router.post(
     '/new',
@@ -31,6 +34,6 @@ router.post(
     ],
     loginUsuario)
 
-router.get('/renew',revalidarToken)
+router.get('/renew',validarJWT,revalidarToken)
 
 module.exports=router;
